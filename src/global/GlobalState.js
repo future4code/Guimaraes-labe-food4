bae3192cfe6a53725721696eea011821fba95324
raulrita08 <raul.vinicius@live.com>
2022-04-26 22:05:30 -0300

    Setup do projeto

---
+++ src/global/GlobalState.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { GlobalStateContext } from "./GlobalStateContext";
+
+const GlobalState = () => {
+    return (
+        <GlobalStateContext.Provider>
+            <div>
+
+            </div>
+        </GlobalStateContext.Provider>
+    )
+}
+
+export default GlobalState;
